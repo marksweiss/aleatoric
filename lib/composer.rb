a8cf5c8a90d8a2d6f25632888eb95bd307b1c7a8
unknown <marksweiss@yahoo.com>
2009-03-18 02:35:16 -0400

    better, hopefully final syntax for adding notes to phrases and players, keywords "add", "notes", "player" and "phrase" implemented

--- lib/composer.rb
+++ lib/composer.rb
@@ -2,57 +2,63 @@ require 'score'
 
 module Aleatoric
 
-$SCORE = ScoreWriter.instance
-$SCORES = {}
-$LAST_SCORE = nil
-
-
-# Score properties, toggle on/off, all on scripts get each note() call added to their Notes
-def on
-  $LAST_SCORE.active? true
-end
-def off
-  $LAST_SCORE.active? false
-end
-# handles keyword "score," puts a new Score into the hash of all active Scores, keyed by name arg
-def score(name)
-  $SCORES[name] = Score.new unless $SCORES.include? name
-  $LAST_SCORE = $SCORES[name]
-end
+# Notes are ordered but are cleared after each add() block, anonymous so just a list, no hash
+@notes = []
+# Flag controls clearing hte notes queue @notes
+@notes_add_flag = true
+# Scores are stored by name and must be ordered, so hash and list
+# Score out is a special case singleton Score, master score writing final output
+@score_out = ScoreWriter.instance
+@scores = {}
+@score_names = []
+# Phrases are stored by name and must be ordered, so hash and list
+@phrases = {}
+@phrase_names = []
+# Players are just stored by name, not ordered, so just hash
+@players = {}
 
 # handles Note file line keyword "note," construct new Note, makes it current Note
 def note(attrs=nil)
-  note = Note.new(attrs)
-  $SCORE << note
-  $SCORES.values.each {|score| score << note if score.active?}
-  note
+  if @notes_add_flag
+    @notes_add_flag = false
+    @notes = []
+  end
+  @notes << Note.new(attrs)
 end
 
 # handles all other Note file line keywords, adds name val as attr to curent Note
-def method_missing(name, val)
-  $SCORE.method_missing_handler(name, val)
-  $SCORES.values.each {|score| score.method_missing_handler(name, val) if score.active?}
+def method_missing(name, *args)
+  #puts name
+  #puts args[0]
+
+  @notes.last.method_missing(name, args[0])
 end
 
-# handles composition client command to load and execute a Note file in the context of this module
-def Aleatoric.load(file)
-  Kernel::load file
+# handles keyword "phrase"
+def phrase(*names)
+  names.each do |name| 
+    @phrases[name] = Phrase.new unless @phrases.include? name
+    @phrase_names << name unless @phrase_names.include? name
+    @phrases[name] << @notes
+  end
 end
 
-# allows composition client to push:
-#  - hash of note params to become Note in current ScoreWriter
-#  - string of hash of note params to become Note in current ScoreWriter
-#  - a Note to append to Notes in current ScoreWriter
-def push(arg=nil)
-  if not arg
-    note arg
-  elsif arg.class == {}.class
-    note arg
-  elsif arg.class == "".class
-    note eval(arg)
-  elsif arg.class == Note.new.class
-    $SCORE << arg
-    $SCORES.values.each {|score| score << arg if score.active?}
+# handles keyword "player"
+def player(*names)
+  names.each do |name| 
+    @players[name] = Player.new unless @players.include? name
+    @players[name] << @notes
+  end
+end
+
+# handles phrases "add notes" or "add phrases" which are then followed by 
+#  block (with not do/end notation) of players and phrases that do the add operation
+#  on the current queue of stuff they care about.
+def notes; :notes; end
+def phrases(*args); :phrases; end
+def add(add_type)
+  if add_type == :notes
+    @notes_add_flag = true   
   end
 end
 
@@ -61,13 +67,25 @@ def csound; :csound; end
 def midi; :midi; end
 def write(format)
   if format == csound
-    # puts $SCORE
-    $SCORES.keys.each {|k| puts "*** Score = #{k} ***\n#{$SCORES[k]}"}
-    $SCORE
+    puts "\nCSound Score\n\n"
+    @score_out.notes.each {|note| puts "#{note}\n"}
+    puts "\nCSound Phrases\n\n"
+    @phrases.values.each {|phrase| phrase.notes.each {|note| puts "#{note}\n"}}
+    puts "\nCSound Players\n\n"
+    @players.values.each {|player| player.notes.each {|note| puts "#{note}\n"}}
   elsif format == midi
-    puts $SCORE
-    $SCORE
+    puts "MIDI Score\n"
+    @score_out.notes.each {|note| puts "#{note}\n"}
+    puts "MIDI Phrases\n"
+    @phrases.values.each {|phrase| phrase.notes.each {|note| puts "#{note}\n"}}
+    puts "CSound Players\n"
+    @players.values.each {|player| player.notes.each {|note| puts "#{note}\n"}}
   end
+end
+
+# handles composition client command to load and execute a Note file in the context of this module
+def load(file)
+  Kernel::load file
 end
 
 end
--- lib/composition.rb
+++ lib/composition.rb
@@ -5,15 +5,23 @@ include Aleatoric
 #  attrs one to a line with whitespace between attr and value), or passing a hash
 #  of note attrs, or passing a string of a hash of note attrs, or passing nil which pushes
 #  a default note onto the score
-load("../test/note.in")
+# Wrap file in module directive so it works but this is hiden from user
+
+file_name = "../test/note.in"
+#file = IO.read(file_name)
+#header = "module Aleatoric\n\n"
+#footer = "\n\nend"
+#open(file_name, 'w') {|f| f << header << file << footer} 
+load file_name
+#open(file_name, 'w') {|f| f << file} 
 
 # Push a default note
-push
+# push
 # Push a new note with these attrs
-push :instrument => 80, :start => 0.0, :duration => 1.0, :amplitude => 1000, :pitch => 8.03
+# push :instrument => 80, :start => 0.0, :duration => 1.0, :amplitude => 1000, :pitch => 8.03
 # Push a new note with these attrs
-push "{:instrument => 90, :start => 0.0, :duration => 2.0, :amplitude => 1200, :pitch => 8.06}"
+# push "{:instrument => 90, :start => 0.0, :duration => 2.0, :amplitude => 1200, :pitch => 8.06}"
 # Push a new note
-push note({:instrument => 100, :start => 0.0, :duration => 1.0, :amplitude => 1300, :pitch => 11.01})
+# push note({:instrument => 100, :start => 0.0, :duration => 1.0, :amplitude => 1300, :pitch => 11.01})
 
 # write csound
