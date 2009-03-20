25337c5798a79aa8a781f71d73cb69d95ef58495
unknown <marksweiss@yahoo.com>
2009-03-20 09:13:45 -0400

    changed logic of note vs. phrase and section, so the latter dont have batch/refresh state, but persist over life of score

--- lib/composer.rb
+++ lib/composer.rb
@@ -4,49 +4,25 @@ require 'singleton'
 module Aleatoric
 
 # State
-class CollectionState
-  attr_accessor :cls
-  attr_reader :items, :items_map
+class BatchState
+  attr_reader :items
   
   def initialize
     @state = false
     @pub_state = false
     @items = []
     @published_items = []
-    @items_map = {}
   end
-  
-  #def cls(cls=nil)
-  #  @cls = cls unless cls == nil
-  #  @cls
-  #end
     
   # Add one or more at a time
   def add(*items)
     items.each {|item| @items << item}
   end
-
-  # Supports client classes that want to store instances by key, assumes the associated values
-  #  should only be stored at all if the key is unique -- otherwise hash and list out of sync
-  def add_by_key(key, item)
-    if not @items_map.include? key
-      @items_map[key] = item
-      add item
-    end
-  end
-  
-  def [](key)
-    return @items_map[key]
-  end
   
   def items
     @items
   end
-  
-  def items_map
-    @items_map
-  end
-  
+    
   def last
     @items.last
   end
@@ -67,7 +43,7 @@ class CollectionState
     @pub_state == true
   end
     
-  def publish  
+  def publish
     # Notes are copied into the published buffer and cleared at
     #  the start of each "add" block
     # Duck-type requirement here, item type @cls must implement dup()
@@ -77,8 +53,85 @@ class CollectionState
   end
   
   def unpublish
-    @pub_state = false    
     @published_items = []
+    @pub_state = false
+  end
+  
+  def subscribe(obj)
+    obj << @published_items
+  end
+  
+  def to_s
+    @items.each {|i| i.to_s}
+    @published_items.each {|i| i.to_s}
+  end
+end
+
+class OngoingState
+  attr_reader :items, :items_map
+  
+  def initialize
+    @state = false
+    @pub_state = false
+    @items = []
+    @published_items = []
+    @items_map = {}
+  end
+  
+  # Add one or more at a time
+  private
+  def add(*items)
+    items.each {|item| @items << item}
+  end
+  public
+  
+  def add_by_key(key, item)
+    if not @items_map.include? key
+      @items_map[key] = item
+      add item
+    end
+  end
+  
+  def [](key)
+    return @items_map[key]
+  end
+  
+  def ordered_keys
+    @items
+  end
+  
+  def items_map
+    @items_map
+  end
+  
+  def last
+    @items_map[@items.last]
+  end
+  
+  def on?
+    @state == true
+  end
+  
+  def on!
+    @state = true
+  end
+  
+  def off!
+    @state = false
+  end
+  
+  def published?
+    @pub_state == true
+  end
+    
+  def publish(*item_keys)
+    @published_items = item_keys.collect {|key| item_map[key].dup}
+    @pub_state = true
+  end
+  
+  def unpublish
+    @published_items = []
+    @pub_state = false    
   end
   
   def subscribe(obj)
@@ -92,18 +145,17 @@ class CollectionState
 end
 
 # State management of entities being declared and added to each other in "add" blocks
-@note_state = CollectionState.new
-@note_state.cls = Note.class
-@phrase_state = CollectionState.new
-@phrase_state.cls = Phrase.class
-@section_state = CollectionState.new
-@section_state.cls = Section.class
+@note_state = BatchState.new
+@phrase_state = OngoingState.new
+@section_state = OngoingState.new
 
 # "add" Event types, these are things that can be added
 @nil_event = :nil_event
 @note_event = :note_event
 @phrase_event = :phrase_event    
-@section_event = :section_event    
+@section_event = :section_event
+
+@score_out = ScoreWriter.instance  
 
 
 ################################################
@@ -159,7 +211,7 @@ def notes
   @note_event
 end
 # handles keyword "phrases" which passes an event indicator to "add"
-def phrases
+def phrases(*names)
   @phrase_event
 end
 
@@ -184,6 +236,39 @@ def add(event)
   end
 end
 
+def to; end
+
+def csound; :csound; end
+# def midi; :midi; end
+def format(fmt)
+  # TODO How to control how phrases and sections are actually ordered?
+  # Probably if you create sections, only they are written, in order
+  #  and if you create phrases then only they are in order
+  #  otherwise notes are, in order
+  if @write_flag and fmt == :csound
+    puts 'PHRASES'  
+    @phrase_state.ordered_keys.each do |k|
+      @score_out << @phrase_state[k]
+      puts "\n" + k.to_s + "\n" + "\n"
+      puts @phrase_state[k].to_s
+    end
+    puts 'SECTIONS'  
+    @section_state.ordered_keys.each do |k|
+      @score_out << @section_state[k]
+      puts "\n" + k.to_s + "\n" + "\n"
+      puts v.to_s
+    end     
+  
+    @write_flag = false
+  end
+  # TODO elsif write_flag == false raise exception, or better block state management
+end
+
+def write(name=nil)
+  @score_out.name = name
+  @write_flag = true
+end
+
 # handles all other Note file line keywords, adds name val as attr to curent Note
 def method_missing(name, *args)
   # TEMP DEBUG
@@ -198,24 +283,6 @@ def method_missing(name, *args)
   @note_state.last.method_missing(name, args[0]) if @note_state.on?
   @phrase_state.last.method_missing(name, args[0]) if @phrase_state.on?
   @section_state.last.method_missing(name, args[0]) if @section_state.on?
-end
-
-def csound; :csound; end
-def format; :format; end
-def write(fmt_map)
-  fmt = fmt_map[:format]
-  if fmt == :csound
-    puts 'PHRASES'  
-    @phrase_state.items_map.each do |k, v|
-      puts "\n" + k.to_s + "\n" + "\n"
-      puts v.to_s
-    end
-    puts 'SECTIONS'  
-    @section_state.items_map.each do |k, v|
-      puts "\n" + k.to_s + "\n" + "\n"
-      puts v.to_s
-    end    
-  end    
 end
 
 end
