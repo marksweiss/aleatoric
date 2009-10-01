7a21d400d67eec758309f04cf5a031bbaca8927c
unknown <marksweiss@yahoo.com>
2009-10-01 00:52:13 -0400

    Added and passed unit tests for new midi.rb. Can create tracks, bind instrument (MIDI preset) to track, add notes to track)

--- lib/midi.rb
+++ lib/midi.rb
@@ -1,11 +1,11 @@
-# Start looking for MIDI module classes in the directory above this one.
-# This forces us to use the local copy, even if there is a previously
-# installed version out there somewhere.
-$LOAD_PATH[0, 0] = File.join(File.dirname(__FILE__), '..', 'lib')
+require 'global'
 
+require 'rubygems'
+
+require 'dl/import'
 require 'midilib/sequence'
 require 'midilib/consts'
-require 'rubygems'
+
 require 'ruby-debug' ; Debugger.start
 
 class AleatoricIllegalMidiOperationException < Exception; end
@@ -13,13 +13,15 @@ class AleatoricIllegalMidiOperationException < Exception; end
 class MidiManager
 
   include MIDI
+  include Aleatoric
 
   attr_reader :name
   
   def initialize(name=nil)
     # custom init steps
     @name = name || 'Sequence Name'
-    @channel_tracks = {}    
+    @channel_tracks = {}
+    @channel_instruments = {}
 
     # midilib init steps
     @seq = Sequence.new()
@@ -30,23 +32,48 @@ class MidiManager
     track.events << MetaEvent.new(META_SEQ_NAME, @name)    
   end
   
+  # NOTE: MIDI doesn't require this, but this interface assumes each track will be
+  #  assigned to one and only one channel.  The interface hides tracks entirely
+  #  (since they are  MIDI concept with no analog in Composer) and to the user
+  #  their Composer script assigns Notes (Composer Notes) to Channels (MIDI channels)
+  #  but even that isn't actually exposed in Composer (other than the abstract
+  #  concept of multiple Channels available if you set format == :midi
   def channel(channel)
     @channel_tracks[channel] ||= Track.new(@seq)
   end
 
+  def channel_nil?(channel)
+    @channel_tracks[channel].nil?
+  end
+  
   def instrument(channel, instrument, delta_time=0)
     self.channel(channel) if channel_nil?(channel)
     @channel_tracks[channel].events << ProgramChange.new(channel, instrument, delta_time)
+    @channel_instruments[channel] = instrument
   end
 
+  def channel_instrument(channel)
+    @channel_instruments[channel]
+  end
+  
   # Args named with MIDI semantics. Converting to Composer semantics:
   #  note == pitch, velocity == amplitude == volume, delta_time == duration  
   def add_note(channel, note, velocity, delta_time) 
-    note_length = @seq.note_to_delta(delta_to_note_str(delta_time))
+    channel(channel) if channel_nil?(channel)
+    note_length = @seq.note_to_delta(delta_to_note_str(delta_time))    
     @channel_tracks[channel].events << NoteOnEvent.new(channel, note, velocity, 0)
     @channel_tracks[channel].events << NoteOffEvent.new(channel, note, velocity, note_length)
   end
   
+  # NOTE: Breaks encapsulation and really only intended for unit testing add_note()
+  def channel_notes(channel)
+    notes = []
+    if not @channel_tracks[channel].nil?
+      @channel_tracks[channel].each {|note| notes << note if note.note?}
+    end
+    notes
+  end
+    
   def save(file_name)
     File.open(file_name, 'wb') {|file| @seq.write file}  
   end
@@ -54,24 +81,13 @@ class MidiManager
   
   private
   
-  def channel_nil?(channel)
-    @channel_tracks[channel].nil?
+  def delta_to_note_str(duration)
+    DUR_TO_NOTE_STR[duration]
   end
-
-  NOTE_STR_TO_DUR	=	{'whole' => WHL, 
-                     'half' => HLF, 
-                     'quarter' => QRTR, 
-                     'eighth' => EITH, 
-                     '8th' => EITH, 
-                     'sixteenth' => SXTNTH, 
-                     '16th' => SXTNTH, 
-                     'thirty second' => THRTYSCND, 
-                     'thirtysecond' => THRTYSCND, 
-                     '32nd' => THRTYSCND, 
-                     'sixty fourth' => SXTYFRTH, 
-                     'sixtyfourth' => SXTYFRTH, 
-                     '64th' => SXTYFRTH}
-
+  alias dur_to_note_str delta_to_note_str
+  alias dur_to_note delta_to_note_str
+  alias delta_to_note delta_to_note_str
+  
   DUR_TO_NOTE_STR	=	{WHL => 'whole', 
                      HLF => 'half', 
                      QRTR => 'quarter', 
@@ -85,19 +101,28 @@ class MidiManager
                      SXTYFRTH => 'sixty fourth', 
                      SXTYFRTH => 'sixtyfourth', 
                      SXTYFRTH => '64th'}                   
-                     
-  note_str_to_delta(note_str)
-    NOTE_STR_TO_DUR[note_str]
-  end
-  alias note_str_to_dur note_str_to_delta
-  alias note_to_dur note_str_to_delta
-  alias note_to_delta note_str_to_delta
+  
+  # We don't need this right now, but it will be annoying to do it over if we ever do  
+  #note_str_to_delta(note_str)
+  #  NOTE_STR_TO_DUR[note_str]
+  #end
+  #alias note_str_to_dur note_str_to_delta
+  #alias note_to_dur note_str_to_delta
+  #alias note_to_delta note_str_to_delta
 
-  delta_to_note_str(duration)
-    DUR_TO_NOTE_STR[duration]
-  end
-  alias dur_to_note_str delta_to_note_str
-  alias dur_to_note delta_to_note_str
-  alias delta_to_note delta_to_note_str
+  # We don't need this right now, but it will be annoying to do it over if we ever do
+  # NOTE_STR_TO_DUR	=	{'whole' => WHL, 
+  #                   'half' => HLF, 
+  #                   'quarter' => QRTR, 
+  #                   'eighth' => EITH, 
+  #                   '8th' => EITH, 
+  #                   'sixteenth' => SXTNTH, 
+  #                   '16th' => SXTNTH, 
+  #                   'thirty second' => THRTYSCND, 
+  #                   'thirtysecond' => THRTYSCND, 
+  #                   '32nd' => THRTYSCND, 
+  #                   'sixty fourth' => SXTYFRTH, 
+  #                   'sixtyfourth' => SXTYFRTH, 
+  #                   '64th' => SXTYFRTH}
   
 end
--- test/midi_test.rb
+++ test/midi_test.rb
@@ -4,20 +4,63 @@ require 'test/unit'
 require 'rubygems'
 require 'ruby-debug' ; Debugger.start
 
-class Midi_Test < Test::Unit::TestCase
+class MidiManager_Test < Test::Unit::TestCase
   # def setup
   # end
   # def teardown
   # end
 
-  def test__filemidi_initialize
-    puts "test__filemidi_initialize ENTERED"
+  def test__initialize
+    puts "test__initialize ENTERED"
 
-    midi = FileMIDI.new
-
+    midi = MidiManager.new
     assert(midi != nil)
-  
-    puts "test__filemidi_initialize COMPLETED"
+    
+    midi_named = MidiManager.new('foo')
+    assert(midi_named != nil && midi_named.name == 'foo')
+    
+    puts "test__initialize COMPLETED"
   end
+  
+  def test__channel_channel_nil
+    puts "test__channel_channel_nil ENTERED"
+
+    midi = MidiManager.new
+    assert(midi != nil)
+    
+    midi.channel 1
+    assert(! midi.channel_nil?(1))
+
+    puts "test__channel_channel_nil COMPLETED"
+  end  
+  
+  def test__instrument_channel_instrument
+    puts "test__instrument ENTERED"
+
+    midi = MidiManager.new
+    assert(midi != nil)
+    
+    midi.instrument(channel=1, instrument=100, delta_time=1.0)
+    assert(midi.channel_instrument(1) == 100)
+
+    puts "test__instrument COMPLETED"
+  end  
+  
+  def test__add_note
+    puts "test__add_note ENTERED"
+
+    midi = MidiManager.new
+    assert(midi != nil)
+    
+    # note here == pitch in MIDI lingo, 64 == C4
+    midi.add_note(channel=1, note=64, velocity=100, delta_time=4.0)
+    midi_notes = midi.channel_notes(1)
+    # length == 2 because this sets NoteOn and NoteOff
+    assert(midi_notes.length == 2)
+    # returns true if event is either NoteOn or NoteOff
+    assert(midi_notes[0].note? && midi_notes[1].note?)
+
+    puts "test__add_note COMPLETED"
+  end  
   
 end
