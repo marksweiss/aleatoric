b1bf39d36f69af71b92c5ecdcc9dc2fd86489a4e
Mark S. Weiss <marksweiss@yahoo.com>
2010-02-22 23:34:52 -0500

    Cleaning up junk

--- lib/conf_csound.rb
+++
@@ -1,5 +0,0 @@
-# Name of the include file of functions generating lookup tables for the note functions
-#  used by the opcodes in the CSound Orchestra
-$csound_score_include_file_name = 'oscil_sine_ftables_1.txt'
-# Name of the CSound Orchestra file with the instrument definitions used in rendering the Score
-$csound_orc_file_name = 'markov_opt_1.orc'
