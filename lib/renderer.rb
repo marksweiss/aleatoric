99d89860c4ff8bebab57248a8d0cf1b264183056
Mark S. Weiss <marksweiss@yahoo.com>
2010-04-15 08:55:06 -0400

    Moved check for CSound path from global.rb to renderer.rb. All tests passed.

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -28,13 +28,25 @@ class Renderer
   public
 
   def render(render_format, out_file_name, score_file_name=nil)
+  
+    # Set the path to CSound. Installer for Mac sets path correctly but Windows does not and you have to add 
+    #  it to PATH yourself (of course).  Rather than add this reasonably arcane and technical step to install 
+    #  instructions we just assume default CSound install path (which hopefully doesn't ever change) and set a global here
+    if include_win?
+      csound_path = %q{C:\Program Files\Csound\bin}
+    elsif include_mac?
+      csound_path = 'csound'
+    else
+      raise "ONLY WINDOWS AND MAC ARE SUPPORTED FOR CSOUND RENDERING AT THIS TIME"    
+    end
+  
     case render_format.to_sym
     when :csound
       # TODO From config, base path, should be a setting used all over project
       if include_win? # RUBY_PLATFORM.include?('mswin')
-        system("#{$CSOUND_PATH} -m0 -d -g -s -W -o#{out_file_name} #{self.orchestra} #{score_file_name}")
+        system("#{csound_path} -m0 -d -g -s -W -o#{out_file_name} #{self.orchestra} #{score_file_name}")
       else
-        system("#{$CSOUND_PATH} -m7 -d -g -s -A -o#{out_file_name} #{self.orchestra} #{score_file_name}")        
+        system("#{csound_path} -m7 -d -g -s -A -o#{out_file_name} #{self.orchestra} #{score_file_name}")        
       end
     when :midi
       # no-op
